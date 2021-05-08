7afa0b069fefd913607e3bfa24dc8b28b7bf2df0
Artur Rybak <arturybak@outlook.com>
2021-05-08 15:55:16 +0100

    backup before upgrading expo

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('logging test');
   return (
     <View style={styles.container}>
       <Text>hey flops</Text>
